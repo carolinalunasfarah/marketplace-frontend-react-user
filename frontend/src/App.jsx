ad16917bce286691377497815fd809280e183ed3
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-02-21 15:46:35 -0300

    create navigation with style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 
 //import Menu from "./components/Menu";
+import Navigation from "./components/Navigation";
 
+import Home from "./views/Home";
 import Cart from "./views/Cart";
 import Error404 from "./views/Error404";
 import Products from "./views/Products";
 import Product from "./views/Product";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 const App = () => {
     return (
         <>
             {/*<Menu />*/}
+            <Navigation />
 
             <Routes>
-                {/*<Route path="/" element={<Home />} />*/}
+                <Route path="/" element={<Home />} />
                 <Route path="/cart" element={<Cart />} />
                 <Route path="/products" element={<Products />} />
                 <Route path="/product/:id_product" element={<Product />} />
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,7 +1,34 @@
-const Navigation = () => {
-    return (
-        <div>Navigation</div>
-    )
-}
+import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import Navbar from "react-bootstrap/Navbar";
 
-export default Navigation
+import { ProductContext } from "../context/ProductContext";
+
+const Navigation = () => {
+    const activeClass = ({ isActive }) => (isActive ? "active" : "inactive");
+    const { emptyCart } = useContext(ProductContext);
+
+    return (
+        <Navbar className="navigation">
+            <section className="navLinks">
+                <NavLink className={activeClass} to="/">
+                    {" "}
+                    Tienda
+                </NavLink>
+                <div>
+                    <NavLink className={activeClass} to="/register">
+                        Crear Cuenta
+                    </NavLink>
+                    <NavLink className={activeClass} to="/login">
+                        Ingresar
+                    </NavLink>
+                    <NavLink className={activeClass} to="/cart">
+                        Carrito: {emptyCart}
+                    </NavLink>
+                </div>
+            </section>
+        </Navbar>
+    );
+};
+
+export default Navigation;
