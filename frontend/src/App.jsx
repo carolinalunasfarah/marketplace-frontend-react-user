cd86d52792bdda8f21af757d526b9ca87347454e
Benjamín Segura Hernández <benjamin.segura.hernandez@gmail.com>
2024-02-20 22:03:33 -0300

    component products + npm i rc-slider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import { Route, Routes } from "react-router-dom";
 
 //import Cart from "./views/Cart";
 import Error404 from "./views/Error404";
-//import Home from "./views/Home";
-//import Producto from "./views/Producto";
+import Products from "./views/Products";
+import Product from "./views/Product";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -13,13 +13,13 @@ import './App.css';
 const App = () => {
     return (
         <>
-
             {/*<Menu />*/}
 
             <Routes>
                 {/*<Route path="/" element={<Home />} />*/}
-                {/*<Route path="/producto/:id_producto" element={<Producto />} />*/}
                 {/*<Route path="/cart" element={<Cart />} />*/}
+                <Route path="/products" element={<Products />} />
+                <Route path="/product/:id_product" element={<Product />} />
                 <Route path="*" element={<Error404 />} />
             </Routes>
         </>
---
+++ frontend/src/data/categories.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        "id_category" : "bienestar",
+        "name"        : "Bienestar"        
+    },
+    {
+        "id_category" : "manualidades",
+        "name"        : "Manualidades"
+    },
+    {
+        "id_category" : "musica",
+        "name"        : "Música"
+    },
+    {
+        "id_category" : "plantas",
+        "name"        : "Plantas"
+    }
+];
