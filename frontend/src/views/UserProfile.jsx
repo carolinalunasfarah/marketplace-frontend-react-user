8b285a4e2de2e285131dd6daf68ed387c1bdf7c5
Valentina <valentinamr@gmail.com>
2024-02-24 11:15:28 -0300

    Add UserProfile
    Falta agregar objetivos de gamificación y un resumen en el inicio de Mi Perfil.

--- frontend/src/views/UserProfile.jsx
+++ frontend/src/views/UserProfile.jsx
@@ -1,7 +1,75 @@
+import { useState, useContext } from "react";
+import { useParams, Link, Outlet } from "react-router-dom"
+import { Container, Row, Col, Image } from "react-bootstrap"
+import { UserContext } from "../context/UserContext"
+
 const UserProfile = () => {
-    return (
-        <div>UserProfile</div>
-    )
+  const { users } = useContext(UserContext)
+  const [isLinkClicked, setIsLinkClicked] = useState(false);
+  const { userId } = useParams()
+  const id = parseInt(userId, 10);
+
+  // Objecto usuario según id
+  const user = users.find(user => user.id_user === id)
+
+  // Gamificación Mi Market Latino
+  let objectives = [false, false, false, false, false];
+  if (user) {
+    // Objetivo 1: usuario completo, al menos una compra, al menos una venta, al menos un favorito
+    objectives[0] = [user.address, user.avatar_url, user.phone].every(value => value !== '' && value !== undefined && value !== null);
+  }
+  const filledStarsCount = objectives.filter(Boolean).length
+  const stars = Array.from({ length: 5 }, (_, index) => (
+    <i key={index} className={`bi bi-star-fill text-primary me-1 ${index < filledStarsCount ? '' : 'opacity-25'}`}></i>
+  ));
+
+  return (
+    <Container fluid className="bg-body-secondary">
+      <Row>
+        <Col className="col-2 d-flex flex-column justify-content-start align-items-center bg-white pt-4 ps-0 m-3 rounded-4 box-shadow">
+          {user.avatar_url ? <Image src={user.avatar_url} width={100} className="rounded-circle" /> : <i className="bi bi-camera fs-1 px-4 py-3 bg-body-secondary rounded-circle"></i>}
+          <h2 className="fs-5 m-2">{user.firstname} {user.lastname}</h2>
+          <p>{user.email}</p>
+          <p>{stars}</p>
+          <hr className="border-2 border-secondary w-75" />
+          <Link to="mis-datos" onClick={() => setIsLinkClicked(true)} className="text-decoration-none px-3 py-2 bg-secondary rounded-3 text-white shadow-sm w-75 my-2">
+            <i className="bi bi-person text-white me-2"></i>Mis Datos
+          </Link>
+          <Link to="mis-favoritos" onClick={() => setIsLinkClicked(true)} className="text-decoration-none px-3 py-2 bg-secondary rounded-3 text-white shadow-sm w-75 my-2">
+            <i className="bi bi-heart text-white me-2"></i>Mis Favoritos
+          </Link>
+          <Link to="mis-productos" onClick={() => setIsLinkClicked(true)} className="text-decoration-none px-3 py-2 bg-secondary rounded-3 text-white shadow-sm w-75 my-2">
+            <i className="bi bi-box2-heart text-white me-2"></i>Mis Productos
+          </Link>
+          <Link to="mis-compras" onClick={() => setIsLinkClicked(true)} className="text-decoration-none px-3 py-2 bg-secondary rounded-3 text-white shadow-sm w-75 my-2">
+            <i className="bi bi-bag text-white me-2"></i>Mis Compras
+          </Link>
+          <Link to="mis-ventas" onClick={() => setIsLinkClicked(true)} className="text-decoration-none px-3 py-2 bg-secondary rounded-3 text-white shadow-sm w-75 my-2">
+            <i className="bi bi-cash-coin text-white me-2"></i>Mis Ventas
+          </Link>
+        </Col>
+        <Col className="col-7 bg-body-tertiary p-4 m-3 rounded-4 box-shadow">
+          {!isLinkClicked ? (
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <h1>¡Hola {user.firstname}!</h1>
+              <p>Este es tu dashboard de <b>{user.role === "registered" ? "usuario" : "administrador"}</b></p>
+              <p>{user.role === "registered" ?
+                "Navega por el menú, cumple los objetivos, gana estrellas y accede a beneficios exclusivos" :
+                "Haz tus cosas de administrador"}</p>
+            </div>
+          ) : (
+            <Outlet context={{ user, setIsLinkClicked }} />
+          )}
+        </Col>
+        <Col className="my-3">
+          <div className="animated-gradient text-white p-3 mb-4 rounded-4 box-shadow text-center">
+            <strong>Hot Deals</strong> en Mi Market Latino
+          </div>
+          <Image src="/ads.webp" className="img-fluid rounded-4" />
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default UserProfile
