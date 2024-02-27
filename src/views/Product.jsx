6c085b9493d70e61566b421728454e96326d3439
Valentina <valentinamr@gmail.com>
2024-02-27 20:11:14 -0300

    Add vendor name to Product View

--- src/views/Product.jsx
+++ src/views/Product.jsx
@@ -19,52 +19,53 @@ import NavigationTrail from "../components/NavigationTrail";
 import Reinsurances from "../components/Reinsurances";
 
 const Product = () => {
-    const {
-        title,
-        products,
-        addToCart,
-        removeFromCart,
-        confirmCart,
-        getQuantityFromCart,
-        formatPrice,
-        getCategory,
-    } = useContext(DataContext);
-    const { id_product } = useParams();
+  const user = {firstname: "Pepe", lastname: "Luna"} // Modificar
+  const {
+    title,
+    products,
+    addToCart,
+    removeFromCart,
+    confirmCart,
+    getQuantityFromCart,
+    formatPrice,
+    getCategory,
+  } = useContext(DataContext);
+  const { id_product } = useParams();
 
-    const product =
-        products[
-            products.findIndex(
-                (product) => Number(product.id_product) === Number(id_product)
-            )
-        ];
+  const product =
+    products[
+    products.findIndex(
+      (product) => Number(product.id_product) === Number(id_product)
+    )
+    ];
 
-    if (!product) {
-        return <Error404 />;
-    }
+  if (!product) {
+    return <Error404 />;
+  }
 
-    useEffect(() => {
-        document.title = `${title} - ${product.name}`;
-    }, []);
+  useEffect(() => {
+    document.title = `${title} - ${product.name}`;
+  }, []);
 
   return (
     <Container fluid className="bg-body-secondary">
       <section className="py-4">
         <NavigationTrail
-                    paths={[
-                        {
-                            text: "Tienda",
-                            to: "/products",
-                        },
-                        {
-                            text: product.name,
-                            active: true,
-                        },
-                    ]}></NavigationTrail>
+          paths={[
+            {
+              text: "Tienda",
+              to: "/products",
+            },
+            {
+              text: product.name,
+              active: true,
+            },
+          ]}></NavigationTrail>
       </section>
       <section className="d-flex justify-content-center pb-4">
         <Row className="row-cols-1 row-cols-md-3 bg-white rounded-4 box-shadow mx-4 py-4">
           <Col className="cols-12 text-center pt-4">
-            <img src={product.image_url} width={300} className="img-fluid"/>
+            <img src={product.image_url} width={300} className="img-fluid" />
           </Col>
           <Col className="col-12 d-flex d-lg-none bg-body-secondary justify-content-between align-items-center py-2">
             <h3>Cantidad</h3>
@@ -80,9 +81,10 @@ const Product = () => {
           </Col>
           <Col className="col-12 pt-4">
             <h1>{product.name}</h1>
+            <p>Vendido por {user.firstname} {user.lastname} </p>
             <p>{product.description}</p>
             <h2 className="fs-4 text-primary fw-bold">Precio: {formatPrice(product.price)}</h2>
-            <hr/>
+            <hr />
             <div className="d-flex justify-content-between">
               <Badge bg="success" className="Category fs-6" data-id_category={product.id_category}>{getCategory(product.id_category, "name")}</Badge>
               <Favorites />
