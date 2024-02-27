c80084da253f6c6e4c2707ea95ee117eb7860dd6
Valentina <valentinamr@gmail.com>
2024-02-27 09:15:41 -0300

    Create componente Favorite to be reused

---
+++ src/components/Favorites.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react"
+import { Button } from 'react-bootstrap'
+import { ProductContext } from "../context/ProductContext"
+
+const Favorites = ({productId}) => {
+  const { favorites, setFavorites } = useContext(ProductContext)
+  const isFavorite = favorites.some(favorite => favorite.id_product === productId)
+
+  const handleFavorite = (productId) => {
+    if (isFavorite) {
+      setFavorites(favorites.filter(favorite => favorite.id_product !== productId))
+    } else {
+      setFavorites([...favorites, { id_user: 1, id_product: productId }])
+    }
+  }
+
+  return (
+    <Button variant="link" className="p-0 border-0 bg-transparent" onClick={() => handleFavorite(productId)}>
+      { isFavorite ? 
+      <i className="bi bi-suit-heart-fill text-primary fs-3"></i>
+      :
+      <i className="bi bi-suit-heart text-primary fs-3"></i>
+      }
+    </Button>
+  )
+}
+
+export default Favorites
