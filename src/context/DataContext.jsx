00f9a97d2a566f651859e72b749ea237829261a4
Valentina <valentinamr@gmail.com>
2024-03-12 22:54:55 -0300

    Change formatDate to dd-mm-yyyy

--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -377,9 +377,16 @@ const DataProvider = ({ children }) => {
         }).format(price);
     };
 
-    const formatDate = (date_add) => {
-        return new Date(date_add).toISOString().split("T")[0];
-    };
+    function formatDate(dateStr) {
+      const date = new Date(dateStr);
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours().toString().padStart(2, '0');
+      const minutes = date.getMinutes().toString().padStart(2, '0');
+      const seconds = date.getSeconds().toString().padStart(2, '0');
+      return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+    }
 
     const formatBytes = (bytes) => {
         if (!bytes) {
