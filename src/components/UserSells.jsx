cfdb53fa92f3c9786730801f3db4e857c44b5384
Valentina <valentinamr@gmail.com>
2024-03-12 22:55:57 -0300

    Update to display orders

--- src/components/UserSells.jsx
+++ src/components/UserSells.jsx
@@ -11,128 +11,131 @@ import { DataContext } from "../context/DataContext";
 import { Row, Col, Button, Image } from "react-bootstrap";
 
 const UserSells = () => {
-    const { setIsLinkClicked } = useOutletContext();
-    const { setUserObjective, sells, getSells, formatPrice, formatDate } =
-        useContext(DataContext);
-    const [visibleDetailId, setVisibleDetailId] = useState(null);
+  const { setIsLinkClicked } = useOutletContext();
+  const { setUserObjective, sells, getSells, formatPrice, formatDate } =
+    useContext(DataContext);
+  const [visibleDetailId, setVisibleDetailId] = useState(null);
 
-    useEffect(() => {
-      getSells();
-        if (sells.length > 0) {
-            setUserObjective((prevState) => ({ ...prevState, hasSells: true }));
-        }
-    }, []);
+  useEffect(() => {
+    getSells();
+    if (sells.length > 0) {
+      setUserObjective((prevState) => ({ ...prevState, hasSells: true }));
+    }
+  }, []);
 
-    const addDaysToDate = (dateStr, daysToAdd) => {
-        const date = new Date(dateStr);
-        date.setDate(date.getDate() + daysToAdd);
-        return date.toISOString().split("T")[0];
-    };
+  const addDaysToDate = (dateStr, daysToAdd) => {
+    const date = new Date(dateStr);
+    date.setDate(date.getDate() + daysToAdd);
 
-    const toggleDetails = (id) => {
-        setVisibleDetailId(visibleDetailId === id ? null : id);
-    };
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // +1 porque los meses son 0-indexados
+    const year = date.getFullYear();
 
-    console.log(sells)
-    return (
-        <>
-            <section>
-                <h1>Mis Ventas</h1>
+    return `${day}-${month}-${year}`;
+  };
 
-                {sells.length === 0 ? (
-                    <p>Realiza tu primera venta y recibe una estrella.</p>
-                ) : (
-                    <p>Revisa el listado de tus ventas y fechas de abono.</p>
-                )}
-            </section>
-            <section>
-                {sells.map((sell) => (
-                    <div
-                        key={sell.id_order}
-                        className="bg-white rounded-4 box-shadow">
-                        <Row className="row-cols-12 row-cols-lg-2 my-4 p-3">
-                            <Col className="col-12 col-lg-8 d-flex flex-row justify-content-start align-items-center gap-4">
-                                <p className="text-primary fw-bold">
-                                    <i className="bi bi-bag-check d-none d-lg-flex"></i>{" "}
-                                    Orden # {sell.id_order}
-                                </p>
-                                <p>
-                                    Comprado el {formatDate(sell.purchase_date)}
-                                </p>
-                                <p>Total:{" "}
-                                        {formatPrice(
-                                            sell.unit_price *
-                                                sell.product_quantity
-                                        )}</p>
-                            </Col>
-                            <Col className="col-12 col-lg-3">
-                                <Button
-                                    className="btn-primar border-0"
-                                    onClick={() =>
-                                        toggleDetails(sell.id_order)
-                                    }>
-                                    {visibleDetailId === sell.id_order
-                                        ? "Ocultar Detalles"
-                                        : "Ver Detalles"}
-                                </Button>
-                            </Col>
-                        </Row>
-                        {visibleDetailId === sell.id_order && (
-                            <Row className="col-12 row-cols-lg-4 d-flex flex-row align-items-center justify-content-between">
-                                <Col className="col-12 col-lg-3 text-center">
-                                    <Image
-                                        src={sell.image_url}
-                                        width={90}
-                                        className="bg-white border border-1 rounded-4 p-2"
-                                    />
-                                </Col>
-                                <Col className="col-12 col-lg-3 text-center py-2">
-                                    <Link
-                                        to={`/producto/${sell.id_product}`}
-                                        className="text-decoration-none text-black">
-                                        <p className="fw-bolder">
-                                            {sell.product_name}{" "}
-                                            <i className="bi bi-search"></i>
-                                        </p>
-                                    </Link>
-                                    <small>
-                                        Cant. {sell.product_quantity}
-                                        <br />
-                                        Subtotal:{" "}
-                                        {formatPrice(
-                                            sell.unit_price *
-                                                sell.product_quantity
-                                        )}
-                                    </small>
-                                </Col>
-                                <Col className="col-12 col-lg-3 text-center py-2">
-                                    <div>
-                                        <small>Estado</small>
-                                        <p className="text-primary fw-bolder">
-                                            Por abonar
-                                        </p>
-                                    </div>
-                                </Col>
-                                <Col className="col-12 col-lg-3 text-center">
-                                    <small>Fecha de abono estimada</small>
-                                    <p className="fw-bolder">
-                                        {addDaysToDate(sell.purchase_date, 4)}
-                                    </p>
-                                </Col>
-                            </Row>
+  const toggleDetails = (id) => {
+    setVisibleDetailId(visibleDetailId === id ? null : id);
+  };
+
+  return (
+    <>
+      <section>
+        <h1>Mis Ventas</h1>
+
+        {sells.length === 0 ? (
+          <p>Realiza tu primera venta y recibe una estrella.</p>
+        ) : (
+          <p>Revisa el listado de tus ventas y fechas de abono.</p>
+        )}
+      </section>
+      <section>
+        {sells.map((sell) => (
+          <div
+            key={sell.id_order}
+            className="bg-white rounded-4 box-shadow">
+            <Row className="row-cols-12 row-cols-lg-2 my-4 mx-0 p-3">
+              <Col className="col-12 col-lg-9 d-flex flex-row justify-content-between align-items-center gap-4 pt-2">
+                <p className="text-primary fw-bold">
+                  <i className="bi bi-bag-check"></i> Orden # {sell.id_order}
+                </p>
+                <p>
+                  Vendido el {formatDate(sell.purchase_date)}
+                </p>
+
+              </Col>
+              <Col className="col-12 col-lg-3">
+                <Button
+                  className="btn-primary border-0"
+                  onClick={() =>
+                    toggleDetails(sell.id_order)
+                  }>
+                  {visibleDetailId === sell.id_order
+                    ? "Ocultar Detalles"
+                    : "Ver Detalles"}
+                </Button>
+              </Col>
+            </Row>
+            {visibleDetailId === sell.id_order && (
+              <Row className="col-12 row-cols-lg-4 d-flex flex-row align-items-center justify-content-between">
+                {sell.products_details.map((product, index) => (
+                  <>
+                    <Col key={`${sell.id_order}-${product.id_product}`} className="col-12 col-lg-3 text-center">
+                      <Image
+                        src={product.image_url}
+                        width={90}
+                        className="bg-white border border-1 rounded-4"
+                      />
+                    </Col>
+                    <Col className="col-12 col-lg-3 text-center py-2">
+                      <Link
+                        to={`/producto/${product.id_product}`}
+                        className="text-decoration-none text-black">
+                        <p className="fw-bolder">
+                          {product.name}{" "}
+                          <i className="bi bi-search"></i>
+                        </p>
+                      </Link>
+                      <small>
+                        Cantidad: {product.product_quantity}
+                        <br />
+                        Subtotal:{" "}
+                        {formatPrice(
+                          product.unit_price *
+                          product.product_quantity
                         )}
-                    </div>
+                      </small>
+                    </Col>
+                    <Col className="col-12 col-lg-3 text-center py-2">
+                      <div>
+                        <small>Estado</small>
+                        <p className="text-primary fw-bolder">
+                          Por abonar
+                        </p>
+                      </div>
+                    </Col>
+                    <Col className="col-12 col-lg-3 text-center">
+                      <small>Fecha de abono estimada</small>
+                      <p className="fw-bolder">
+                        {addDaysToDate(sell.purchase_date, 4)}
+                      </p>
+                    </Col>
+                  </>
                 ))}
-            </section>
-            <section className="d-flex justify-content-end mt-4">
-                <Link
-                    className="bg-transparent text-black border-0"
-                    onClick={() => setIsLinkClicked(false)}>
-                    <i className="bi bi-arrow-left me-1"></i>Volver a Mi Perfil
-                </Link>
-            </section>
-        </>
-    );
+              </Row>
+            )}
+          </div>
+        ))}
+      </section>
+      <section className="d-flex justify-content-end mt-4">
+        <Link
+          className="bg-transparent text-black border-0"
+          onClick={() => setIsLinkClicked(false)}>
+          <i className="bi bi-arrow-left me-1"></i>Volver a Mi Perfil
+        </Link>
+      </section>
+    </>
+  );
 };
 
 export default UserSells;
